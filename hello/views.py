00f28f23e19e6933e3c894df0a00001016bb7e91
ellemonke <ellemonke@gmail.com>
2020-03-12 17:34:58 -0700

    index test

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+    return HttpResponse('Stuck! ' * times)
 
 
 def db(request):
